5212009193d01e1323b17d1ce76a026e9f43050b
Carson Black <uhhadd@gmail.com>
2019-08-20 21:46:18 -0400

    url stuff i guess

--- code/mainwindow.rb
+++ code/mainwindow.rb
@@ -3,12 +3,16 @@
 require 'gtk3'
 require 'gdk3'
 
+require './code/keyring'
+
 $main_builder = nil
 
 def show_main_window()
     $main_builder = Gtk::Builder.new
     $main_builder.add_from_file "resources/ui/main.glade"
     window = $main_builder.get_object "MainWindow"
+    headerbar = $main_builder.get_object "MainHeaderbar"
+    headerbar.subtitle = keyring_get_user_pass()[2]
 
     $main_builder.connect_signals { |handler| method(handler) }
 
